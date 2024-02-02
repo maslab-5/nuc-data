dfb9f0b5cea430d61e974958a1ca338cba692a1c
MutantKraken <mfacton1@gmail.com>
2024-02-02 08:31:07 -0500

    test

--- comp/main.py
+++ comp/main.py
@@ -66,7 +66,7 @@ def init():
 
     command.moveServo(Servo.Camera, 275)
     command.moveServo(Servo.Gate, 310)
-    command.moveServo(Servo.LeftChute, 42)
+    command.moveServo(Servo.LeftChute, 39)
     command.moveServo(Servo.RightChute, 524)
 
     command.motorMove(SmallMotor.Gate, 0, 100)
