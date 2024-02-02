a2fe93cc4022e5215b2a4e39c8b80b9cf17679ff
MutantKraken <mfacton1@gmail.com>
2024-02-02 04:11:26 -0500

    init

---
+++ comp/actions.py
@@ -0,0 +1,292 @@
+from command import Movement, Servo, SmallMotor, LargeMotor, Switch
+
+import math
+import time
+
+class Actions:
+    def __init__(self, map, command, visual, unitLength, unitRotation, maxLoop, dropTime):
+        self.map = map
+        self.com = command
+        self.vis = visual
+
+        self.unitLength = unitLength
+        self.unitRotation = unitRotation
+
+        self.maxLoop = maxLoop
+        self.startTime = time.time()
+        self.dropTime = dropTime
+
+    def waitStop(self):
+        while self.com.isMoving():
+            time.sleep(1/self.maxLoop)
+
+    def turnAbsolute(self, movement, angAbsolute):
+        reached = False
+        finishTime = None
+        scale = self.unitRotation/(2.1*math.pi)
+        while True:
+            x, y, ang = self.com.getPosition()
+            change = angAbsolute - ang
+            if abs(change) < 0.017:
+                if not reached:
+                    finishTime = time.time() + 0.5
+                    reached = True
+                elif time.time() >= finishTime:
+                    break
+
+            goal = change*scale
+
+            if movement is not Movement.Spin:
+                goal *= 2
+
+            self.com.startMovement(movement, int(goal))
+
+            time.sleep(0.25)
+
+    def turnToBearing(self, movement, direction, bearing):
+        x, y, ang = self.com.getPosition()
+        if direction:
+            while bearing > ang:
+                bearing -= 2*math.pi
+            while bearing < ang:
+                bearing += 2*math.pi
+        else:
+            while bearing < ang:
+                bearing += 2*math.pi
+            while bearing > ang:
+                bearing -= 2*math.pi
+
+        self.turnAbsolute(movement, bearing)
+
+    def turnToBearingNearest(self, movement, bearing):
+        x, y, ang = self.com.getPosition()
+
+        while bearing < ang - math.pi:
+            bearing += 2*math.pi
+        while bearing >= ang + math.pi:
+            bearing -= 2*math.pi
+
+        self.turnAbsolute(movement, bearing)
+
+        # self.turnToBearing(movement, offset > 0, bearing)
+
+    def turnToPosition(self, movement, direction, x, y):
+        robX, robY, ang = self.com.getPosition()
+
+        dX = self.unitLength*x - robX
+        dY = self.unitLength*y - robY
+
+        self.turnToBearing(movement, direction, math.atan2(dY, dX))
+    
+    def turnToPositionNearest(self, movement, x, y):
+        robX, robY, ang = self.com.getPosition()
+
+        dX = self.unitLength*x - robX
+        dY = self.unitLength*y - robY
+
+        self.turnToBearingNearest(movement, math.atan2(dY, dX))
+
+    def moveToPosition(self, x, y, dir):
+        robX, robY, ang = self.com.getPosition()
+        dist = math.sqrt(pow(robX-x*self.unitLength, 2) + pow(robY-self.unitLength*y, 2))
+        if not dir:
+            dist = -dist;
+        
+        self.com.startMovement(Movement.Line, int(dist))
+        self.waitStop()
+
+    def turnToStack(self, movement, index):
+        x = self.map.stacks[index][0]
+        y = self.map.stacks[index][1]
+
+        self.turnToPositionNearest(movement, x, y)
+
+    def moveUptoStack(self, index):
+        x = self.map.stacks[index][0]
+        y = self.map.stacks[index][1]
+
+        robX, robY, ang = self.com.getPosition()
+        dist = math.sqrt(pow(robX-x*self.unitLength, 2) + pow(robY-y*self.unitLength, 2))
+        
+        self.com.startMovement(Movement.Line, max(0, int(dist-600)))
+        self.waitStop()
+
+    def alignCamera(self):
+        self.com.setParameters(0.35, 0.0075)
+        time.sleep(0.5)
+        if self.vis.getPercent(not self.map.primaryRed) < 2.4:
+            self.com.startMovement(Movement.Spin, -int(self.unitRotation/8))
+            self.waitStop()
+        if self.vis.getPercent(not self.map.primaryRed) < 2.4:
+            self.com.startMovement(Movement.Spin, int(self.unitRotation/4))
+            self.waitStop()
+        while True:
+            median_x = self.vis.getMedianX(not self.map.primaryRed)
+
+            scale = 0.45
+
+            if abs(median_x) < 8:
+                self.com.stopMovement()
+                break
+
+            self.com.startMovement(Movement.Spin, int(median_x*scale))
+
+            time.sleep(0.25)
+
+    def grabStack(self, index):
+        self.com.motorMove(SmallMotor.Gate, 0, 100)
+        time.sleep(1.5)
+        self.com.motorMove(SmallMotor.Gate, 0, 0)
+        self.com.setParameters(0.3, 0.0075)
+        angle = 0
+        self.com.moveServo(Servo.Camera, 0)
+        self.com.startMovement(Movement.Line, 1500)
+        while True:
+            if not self.com.isMoving():
+                break
+
+            per = self.vis.getPercent(not self.map.primaryRed)
+
+            if per >= 30:
+                angle = self.vis.getAngle()
+                self.com.setParameters(0.2, 0.0006)
+                self.com.startMovement(Movement.Line, 240)
+                break
+
+            time.sleep(1/self.maxLoop)
+        
+        self.waitStop()
+        x, y, ang = self.com.getPosition()
+        fixedX = self.map.stacks[index][0]*self.unitLength
+        fixedY = self.map.stacks[index][1]*self.unitLength
+        self.com.setPosition(fixedX, fixedY, ang)
+
+        if abs(angle) > math.pi/5:
+            self.com.startMovement(Movement.Spin, 200)
+            self.waitStop()
+        
+        self.com.setParameters(0.4, 0.00075)
+        self.com.moveServo(Servo.Camera, 275)
+        self.com.motorMove(SmallMotor.Gate, 100, 0)
+        time.sleep(2)
+        self.com.motorMove(SmallMotor.Gate, 0, 0)
+
+    def sortStack(self):
+        self.com.setParameters(1, 0.00075)
+        self.com.setMotorEnable(LargeMotor.Lift, 1)
+        x, y, ang = self.com.getPosition()
+        primary = not self.map.primaryRed
+        for i in range(3):
+            self.com.setMotorDirection(LargeMotor.Lift, 0)
+            self.com.setMotorSpeed(LargeMotor.Lift, 50)
+            self.com.moveServo(Servo.Gate, 100)
+
+            #TURN
+            ##############
+            self.com.startMovement(Movement.Line, 200)
+            time.sleep(0.6)
+
+            movement = None
+            estimate = None
+            if primary:
+                ang += math.pi/2
+                movement = Movement.ArcRight
+                estimate = self.unitRotation
+            else:
+                ang -= math.pi/2
+                movement = Movement.ArcLeft
+                estimate = -self.unitRotation
+
+            #start with estimate
+            self.com.moveServo(Servo.Gate, 300)
+            self.com.startMovement(movement, estimate)
+            time.sleep(0.25)
+
+            #finish with gyro
+            self.turnAbsolute(movement, ang)
+            ##############
+            primary = not primary
+
+            self.com.setMotorDirection(LargeMotor.Lift, 1)
+            time.sleep(0.75)
+            self.com.startMovement(Movement.Line, 75)
+            time.sleep(0.5)
+            self.com.setMotorDirection(LargeMotor.Lift, 0)
+            self.waitStop()
+            time.sleep(0.25)
+            self.com.setMotorSpeed(LargeMotor.Lift, 0)
+
+        self.com.setParameters(0.4, 0.00075)
+        self.com.setMotorEnable(LargeMotor.Lift, 0)
+
+    def dropGround(self):
+        self.com.setMotorEnable(LargeMotor.Lift, 0)
+        time.sleep(1)
+        self.com.moveServo(Servo.LeftChute, 550)
+        self.com.setParameters(0.2, 0.00075)
+        time.sleep(1)
+        self.com.startMovement(Movement.Line, 600)
+        self.waitStop()
+
+        self.com.moveServo(Servo.LeftChute, 50)
+        self.com.setParameters(0.6, 0.00075)
+        time.sleep(0.5)
+
+    def waitDrop(self):
+        while time.time() - self.startTime < self.dropTime:
+            time.sleep(0.1)
+
+    def dropPlatform(self):
+        self.com.moveServo(Servo.LeftChute, 45)
+        self.com.moveServo(Servo.RightChute, 530)
+        self.com.startMovement(Movement.Line, 100)
+        self.waitStop()
+
+        self.com.setMotorEnable(LargeMotor.Lift, 1)
+        self.com.setMotorDirection(LargeMotor.Lift, 0)
+        self.com.setMotorSpeed(LargeMotor.Lift, 55)
+        time.sleep(1)
+        self.com.setMotorSpeed(LargeMotor.Chute, 100)
+        time.sleep(7.4)
+        self.com.setMotorSpeed(LargeMotor.Chute, 0)
+        self.com.setParameters(0.3, 0.00075)
+        self.com.setMotorCurrent(LargeMotor.Left, 20)
+        self.com.setMotorCurrent(LargeMotor.Right, 20)
+        self.com.startMovement(Movement.Line, -700)
+        self.waitStop()
+
+        self.com.setMotorCurrent(LargeMotor.Chute, 11)
+        self.com.setMotorDirection(LargeMotor.Chute, 0)
+        self.com.setMotorSpeed(LargeMotor.Chute, 17)
+        time.sleep(6)
+        self.com.setMotorSpeed(LargeMotor.Chute, 0)
+        self.com.setMotorSpeed(LargeMotor.Lift, 25)
+        self.com.setMotorDirection(LargeMotor.Lift, 1)
+        time.sleep(1)
+        self.com.setMotorEnable(LargeMotor.Lift, 0)
+        self.com.setMotorSpeed(LargeMotor.Lift, 0)
+        self.com.moveServo(Servo.RightChute, 0)
+        self.com.moveServo(Servo.LeftChute, 550)
+        self.com.stopMovement()
+        self.com.resetPID()
+        time.sleep(0.1)
+        self.com.setMotorCurrent(LargeMotor.Left, 80)
+        self.com.setMotorCurrent(LargeMotor.Right, 80)
+        time.sleep(1)
+        self.com.startMovement(Movement.Line, 600)
+        self.com.setMotorCurrent(LargeMotor.Chute, 100)
+        time.sleep(0.2)
+        self.com.setMotorDirection(LargeMotor.Chute, 1)
+        self.com.setMotorSpeed(LargeMotor.Chute, 80)
+        time.sleep(1)
+        self.com.setMotorSpeed(LargeMotor.Chute, 0)
+        self.com.setMotorDirection(LargeMotor.Chute, 0)
+        self.waitStop()
+
+        self.com.moveServo(Servo.RightChute, 520)
+        self.com.moveServo(Servo.LeftChute, 50)
+
+        self.com.setMotorSpeed(LargeMotor.Chute, 100)
+        while not self.com.getSwitch(Switch.ChuteLimit):
+            time.sleep(1/self.maxLoop)
+        self.com.setMotorSpeed(LargeMotor.Chute, 0)
---
+++ comp/main.py
@@ -0,0 +1,137 @@
+from map import Map
+from camera import Camera
+from command import Com, SmallMotor, LargeMotor, Servo, Switch, Movement
+from actions import Actions
+from visual import Visual
+
+import math
+import time
+import os
+
+running_nuc = os.name != 'nt'
+
+preBlur = 9
+postBlur = 7
+resizeWidth = 256
+resizeHeight = 192
+
+green_range = [[40, 40, 40], [80, 255, 255]]
+red_range = [[-10, 120, 70], [10, 255, 255]]
+
+unitLength = 1452
+unitRotation = 1575
+
+camera_index = 1
+if running_nuc:
+    camera_index = 0
+
+camera_bias_x = -8
+
+roughStartX = 4.25
+roughStartY = 1.75
+# roughStartX = 4
+# roughStartY = 2
+
+
+stackOrder = [2, 4, 3, 1, 0]
+maxLoop = 20
+dropTime = 150
+
+map = Map("mapright.txt", roughStartX, roughStartY)
+camera = Camera(camera_index, resizeWidth, resizeHeight, preBlur, postBlur)
+command = Com(115200)
+visual = Visual(camera, camera_bias_x, green_range, red_range)
+act = Actions(map, command, visual, unitLength, unitRotation, maxLoop, dropTime)
+
+def init():
+    command.startGyroCal(5000)
+    while not command.isGyroCal():
+        time.sleep(1/maxLoop)
+
+    command.setPosition(map.roughStartX*unitLength, map.roughStartY*unitLength, map.startAng)
+
+    command.setMotorEnable(LargeMotor.Lift, 1)
+    command.setMotorDirection(LargeMotor.Lift, 0)
+    command.setMotorCurrent(LargeMotor.Lift, 22)
+    command.setMotorEnable(LargeMotor.Chute, 1)
+    command.setMotorDirection(LargeMotor.Chute, 1)
+    command.setMotorCurrent(LargeMotor.Chute, 100)
+
+    command.setParameters(0.6, 0.00075)
+
+    command.moveServo(Servo.Camera, 275)
+    command.moveServo(Servo.Gate, 100)
+    command.moveServo(Servo.LeftChute, 50)
+    command.moveServo(Servo.RightChute, 520)
+
+    command.motorMove(SmallMotor.Gate, 0, 100)
+    command.setMotorSpeed(LargeMotor.Lift, 50)
+    command.setMotorSpeed(LargeMotor.Chute, 100)
+
+    time.sleep(1.5)
+
+    command.motorMove(SmallMotor.Gate, 0, 0)
+    command.setMotorDirection(LargeMotor.Chute, 0)
+    command.setMotorDirection(LargeMotor.Lift, 0)
+
+    time.sleep(0.25)
+
+    command.setMotorEnable(LargeMotor.Lift, 0)
+    command.setMotorSpeed(LargeMotor.Lift, 0)
+
+    while not command.getSwitch(Switch.ChuteLimit):
+        time.sleep(1/maxLoop)
+
+    command.setMotorSpeed(LargeMotor.Chute, 0)
+    command.setMotorDirection(LargeMotor.Chute, 1)
+
+
+init()
+
+act.turnToStack(Movement.Spin, stackOrder[0])
+act.moveUptoStack(stackOrder[0])
+act.alignCamera()
+act.grabStack(stackOrder[0])
+act.turnToPositionNearest(Movement.Spin, 1.5, 0)
+act.sortStack()
+
+act.turnToStack(Movement.Spin, stackOrder[1])
+act.moveUptoStack(stackOrder[1])
+act.alignCamera()
+act.grabStack(stackOrder[1])
+act.turnToPositionNearest(Movement.Spin, 2.5, 2.5)
+act.sortStack()
+
+act.turnToStack(Movement.Spin, stackOrder[2])
+act.moveUptoStack(stackOrder[2])
+act.alignCamera()
+act.grabStack(stackOrder[2])
+act.turnToPositionNearest(Movement.Spin, 2.5, 2.5)
+act.sortStack()
+
+# act.turnToStack(Movement.Spin,stackOrder[3])
+# act.moveUptoStack(stackOrder[3])
+# act.alignCamera()
+# act.grabStack(stackOrder[3])
+# act.sortStack()
+
+# act.turnToStack(Movement.Spin, stackOrder[4])
+# act.moveUptoStack(stackOrder[4])
+# act.alignCamera()
+# act.grabStack(stackOrder[4])
+# act.sortStack()
+
+
+
+act.turnToPositionNearest(Movement.Spin, 2, 2)
+act.moveToPosition(2, 2, 1)
+act.turnToBearngNearest(Movement.Spin, math.pi)
+act.dropGround()
+
+# act.waitDrop()
+act.turnToPositionNearest(Movement.Spin, 0.5, 2.5)
+act.moveToPosition(0.5, 2.5, 1)
+act.turnToBearingNearest(Movement.Spin, math.pi)
+act.dropPlatform()
+
+camera.destroy()
