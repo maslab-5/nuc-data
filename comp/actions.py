131eade80dbd44b36aa731ffcb57289da3f709c8
MutantKraken <mfacton1@gmail.com>
2024-02-02 07:59:00 -0500

    test

--- comp/actions.py
+++ comp/actions.py
@@ -147,7 +147,7 @@ class Actions:
 
             per = self.vis.getPercent(not self.map.primaryRed)
 
-            if per >= 34:
+            if per >= 26:
                 angle = self.vis.getAngle()
                 self.com.setParameters(0.2, 0.0006)
                 self.com.startMovement(Movement.Line, 232)
